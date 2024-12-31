c4cc00154eb418689c97efe03205440361ea2815
Carlos Andrés <74998389+capb90@users.noreply.github.com>
2024-12-31 09:10:41 -0500

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'backend-wallet',
+  preset: './jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/backend-wallet',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
